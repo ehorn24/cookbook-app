98a14d86c55c3539cec20cd387d5a22e827921ef
Ella <52179940+ehorn24@users.noreply.github.com>
2020-03-04 07:36:31 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { createUser } from "./Services/createUser";
 import { userAuth } from "./Services/userAuth";
 import { Redirect } from "react-router-dom";
 import { getAllRecipes } from "./Services/getAllRecipes";
 import { getAllUsers } from "./Services/getAllUsers";
 import { editProfile } from "./Services/editProfile";
+import { getSaves } from "./Services/getSaves";
+import { newSave } from "./Services/newSave";
+import { postNewRecipe } from "./Services/postNewRecipe";
+import { unsave } from "./Services/unsave";
+import { deleteRecipe } from "./Services/deleteRecipe";
 
+//components
 import HomePage from "./Components/HomePage/HomePage";
 import LoginOrSignup from "./Components/Account/LoginOrSignup";
 import Signup from "./Components/Account/Signup";
 import Login from "./Components/Account/Login";
 import ActivityFeed from "./Components/Feed/ActivityFeed";
 import Profile from "./Components/Profile/Profile";
-import Navigation from "./Components/HomePage/Navigation";
+import Navigation from "./Components/Navigation/Navigation";
 import Recipe from "./Components/Recipe/Recipe";
-import Stars from "./Components/Stars/Stars";
+import Saves from "./Components/Saves/Saves";
 import EditProfilePg from "./Components/Profile/EditProfilePg";
 import AddRecipe from "./Components/Recipe/AddRecipe";
+import SearchResults from "./Components/Search/SearchResults";
+import NotAuth from "./Components/Errors/NotAuth";
+import DoesntExist from "./Components/Errors/DoesntExist";
 
 export default class App extends Component {
   state = {
@@ -31,7 +40,11 @@ export default class App extends Component {
     loggedin: false,
     redirecttofeed: false,
     recipes: [],
-    users: []
+    users: [],
+    savedrecipes: [],
+    searchterm: "",
+    usersearchresults: [],
+    recipesearchresults: []
   };
 
   clearState = e => {
@@ -41,21 +54,34 @@ export default class App extends Component {
       username: "",
       password: "",
       confirmpassword: "",
+      profilepicture: "",
+      profilebio: "",
       loggedin: false,
       redirecttofeed: false,
       recipes: [],
-      users: []
+      users: [],
+      savedrecipes: [],
+      searchterm: "",
+      usersearchresults: [],
+      recipesearchresults: []
     });
   };
 
   componentDidMount() {
     this.getRecipes();
     this.getUsers();
+    this.getAllSaves();
     if (typeof Storage !== undefined) {
       if (localStorage.getItem("username") !== null) {
         this.setState({
           username: localStorage.getItem("username"),
-          loggedin: !!localStorage.getItem("loggedin")
+          loggedin: !!localStorage.getItem("loggedin"),
+          usersearchresults: JSON.parse(
+            localStorage.getItem("usersearchresults")
+          ),
+          recipesearchresults: JSON.parse(
+            localStorage.getItem("recipesearchresults")
+          )
         });
       }
     }
@@ -65,20 +91,15 @@ export default class App extends Component {
     localStorage.state = JSON.stringify(this.state);
   }
 
-  getRecipes = () => {
-    getAllRecipes().then(recipes => {
-      this.setState({ recipes });
-    });
+  handleFormChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
   };
 
+  //user-related (account, signup/login)
   getUsers = () => {
     getAllUsers().then(users => {
       this.setState({ users });
     });
-  };
-
-  handleFormChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
   };
 
   handleSignupSubmit = e => {
@@ -124,6 +145,7 @@ export default class App extends Component {
               }
             }
           );
+          window.location.href = "/feed";
         } else {
           window.alert(
             "Sorry, looks like something is missing. Please make sure you filled out all the fields."
@@ -137,7 +159,6 @@ export default class App extends Component {
     e.preventDefault();
     userAuth(this.state.username, this.state.password).then(res => {
       if (res === "OK") {
-        console.log(res);
         this.setState(
           {
             firstname: "",
@@ -154,6 +175,7 @@ export default class App extends Component {
             }
           }
         );
+        window.location.href = "/feed";
       } else {
         window.alert("Invalid credentials.");
       }
@@ -162,10 +184,7 @@ export default class App extends Component {
 
   logOut = e => {
     e.preventDefault();
-    this.setState({
-      loggedin: false,
-      username: ""
-    });
+    this.clearState();
     if (typeof Storage !== "undefined") {
       if (localStorage.getItem("username") !== "") {
         localStorage.removeItem("username");
@@ -175,13 +194,142 @@ export default class App extends Component {
     window.location.href = "/";
   };
 
+  //search-related
+  searchForUsers = (arr, query) => {
+    let userResult = [];
+    for (let i = 0; i < arr.length; i++) {
+      if (
+        query.some(
+          word =>
+            JSON.stringify(arr[i].firstname.toLowerCase()).includes(word) ||
+            JSON.stringify(arr[i].lastname.toLowerCase()).includes(word) ||
+            JSON.stringify(arr[i].username.toLowerCase()).includes(word)
+        )
+      ) {
+        userResult.push(arr[i]);
+      }
+    }
+    return userResult;
+  };
+
+  searchForRecipes = (arr, query) => {
+    let recipeResult = [];
+    for (let i = 0; i < arr.length; i++) {
+      if (
+        query.some(word =>
+          JSON.stringify(arr[i].recipename.toLowerCase()).includes(word)
+        )
+      ) {
+        recipeResult.push(arr[i]);
+      }
+    }
+    return recipeResult;
+  };
+
+  handleSearchSubmit = e => {
+    e.preventDefault();
+    const searchFor = this.state.searchterm.split(" ");
+    const getUsers = this.state.users;
+    const getRecipes = this.state.recipes;
+    const userResults = this.searchForUsers(getUsers, searchFor);
+    const recipeResults = this.searchForRecipes(getRecipes, searchFor);
+    this.setState(
+      {
+        usersearchresults: [...userResults],
+        recipesearchresults: [...recipeResults]
+      },
+      () => {
+        if (typeof Storage !== "undefined") {
+          localStorage.setItem(
+            "usersearchresults",
+            JSON.stringify(this.state.usersearchresults)
+          );
+          localStorage.setItem(
+            "recipesearchresults",
+            JSON.stringify(this.state.recipesearchresults)
+          );
+        }
+      }
+    );
+    window.location.href = `/search`;
+  };
+
+  //post-recipe related
+  getRecipes = () => {
+    getAllRecipes().then(recipes => {
+      this.setState({ recipes });
+    });
+  };
+
+  handleRecipeSubmit = (
+    username,
+    recipename,
+    recipephoto,
+    [...ingredients],
+    [...steps]
+  ) => {
+    postNewRecipe(
+      username,
+      recipename,
+      recipephoto,
+      [...ingredients],
+      [...steps]
+    ).then(res => {
+      if (!res.error) {
+        this.setState({
+          recipes: [...this.state.recipes, res]
+        });
+        window.location.href = `/recipe/${recipename}`;
+      } else {
+        window.alert("Sorry, that didn't post correctly. Please try again.");
+      }
+    });
+  };
+
+  handleDeleteRecipe = id => {
+    deleteRecipe(id).then(res => {
+      if (!res.error) {
+        const recipes = this.state.recipes.filter(r => !(r.id === id));
+        this.setState({ recipes });
+      }
+    });
+    window.location.href = `/profile/${this.state.username}`;
+  };
+
+  //saved recipe-related
+  getAllSaves = () => {
+    getSaves().then(sRec => {
+      this.setState({ savedrecipes: sRec });
+    });
+  };
+
+  handleSaveSubmit = (recipe_id, user_saved) => {
+    newSave(recipe_id, user_saved).then(res => {
+      if (!res.error) {
+        this.setState({ savedrecipes: [...this.state.savedrecipes, res] });
+      }
+    });
+  };
+
+  handleDeleteSave = (recipe_id, user_saved) => {
+    unsave(recipe_id, user_saved).then(res => {
+      if (!res.error) {
+        const savedrecipes = this.state.savedrecipes.filter(
+          r => !(r.recipe_id === recipe_id && r.user_saved === user_saved)
+        );
+        this.setState({ savedrecipes });
+      }
+    });
+  };
+
+  //feed-related
   renderFeed = () => {
     if (this.state.redirecttofeed) {
       return <Redirect to="/feed" />;
     }
   };
 
-
+  //profile-related
   handleEditProfile = (e, id) => {
     e.preventDefault();
     const {
@@ -233,119 +381,173 @@ export default class App extends Component {
           logOut={this.logOut}
           currentUser={this.state.username}
         />
-        <Route
-          exact
-          path="/"
-          render={props => (
-            <>
-              <HomePage {...props} isLoggedIn={this.state.loggedin} />
-            </>
-          )}
-        />
-        <Route
-          exact
-          path="/loginsignup"
-          render={props => (
-            <>
-              <LoginOrSignup {...props} />
-            </>
-          )}
-        />
-        <Route
-          exact
-          path="/signup"
-          render={props => (
-            <>
-              <Signup
-                userData={this.state}
-                handleFormChange={this.handleFormChange}
-                handleSignupSubmit={this.handleSignupSubmit}
-                clearState={this.clearState}
-                {...props}
-              />
-            </>
-          )}
-        />
-        <Route
-          exact
-          path="/login"
-          render={props => (
-            <>
-              <Login
-                userData={this.state}
-                handleFormChange={this.handleFormChange}
-                handleLoginSubmit={this.handleLoginSubmit}
-                clearState={this.clearState}
-                {...props}
-              />
-            </>
-          )}
-        />
-        <div>
-          {this.renderFeed()}
+        <Switch>
           <Route
             exact
-            path="/feed"
+            path="/"
             render={props => (
               <>
-                <ActivityFeed
-                  currentUser={this.state.username}
-                  recipes={this.state.recipes}
-                  allUsers={this.state.users}
+                <HomePage {...props} isLoggedIn={this.state.loggedin} />
+              </>
+            )}
+          />
+          <Route
+            exact
+            path="/loginsignup"
+            render={props => (
+              <>
+                <LoginOrSignup {...props} />
+              </>
+            )}
+          />
+          <Route
+            exact
+            path="/signup"
+            render={props => (
+              <>
+                <Signup
+                  userData={this.state}
+                  handleFormChange={this.handleFormChange}
+                  handleSignupSubmit={this.handleSignupSubmit}
+                  clearState={this.clearState}
+                  {...props}
                 />
               </>
             )}
           />
-        </div>
-        <Route
-          exact
-          path="/profile/:username"
-          render={props => (
+          <Route
+            exact
+            path="/login"
+            render={props => (
+              <>
+                <Login
+                  userData={this.state}
+                  handleFormChange={this.handleFormChange}
+                  handleLoginSubmit={this.handleLoginSubmit}
+                  clearState={this.clearState}
+                  {...props}
+                />
+              </>
+            )}
+          />{" "}
+          {this.state.loggedin ? (
             <>
-              <Profile
-                {...props}
-                currentUser={this.state.username}
-                recipes={this.state.recipes}
-                users={this.state.users}
+              {this.renderFeed()}
+              <Route
+                exact
+                path="/feed"
+                render={props => (
+                  <>
+                    <ActivityFeed
+                      currentUser={this.state.username}
+                      recipes={this.state.recipes}
+                      allUsers={this.state.users}
+                      handleSearchChange={this.handleFormChange}
+                      searchTerm={this.state.searchterm}
+                      handleSearchSubmit={this.handleSearchSubmit}
+                    />
+                  </>
+                )}
+              />
+
+              <Route
+                exact
+                path="/profile/:username"
+                render={props => (
+                  <>
+                    <Profile
+                      {...props}
+                      currentUser={this.state.username}
+                      recipes={this.state.recipes}
+                      users={this.state.users}
+                    />
+                  </>
+                )}
+              />
+              <Route
+                exact
+                path="/recipe/:recipename"
+                render={props => (
+                  <>
+                    <Recipe
+                      {...props}
+                      currentUser={this.state.username}
+                      showRecipe={this.state.recipes}
+                      handleSave={this.handleSaveSubmit}
+                      allSaves={this.state.savedrecipes}
+                      handleDeleteSave={this.handleDeleteSave}
+                      handleDeleteRecipe={this.handleDeleteRecipe}
+                    />
+                  </>
+                )}
+              />
+              <Route
+                exact
+                path="/profile/:username/saved"
+                render={props => (
+                  <Saves
+                    currentUser={this.state.username}
+                    yourSaves={this.state.savedrecipes.filter(
+                      s => s.user_saved === this.state.username
+                    )}
+                    allRecipes={this.state.recipes}
+                  />
+                )}
+              />
+              <Route
+                exact
+                path="/profile/:username/edit"
+                render={props => (
+                  <EditProfilePg
+                    userData={this.state}
+                    userId={
+                      this.state.users.filter(
+                        user => user.username === this.state.username
+                      )[0]
+                    }
+                    handleFormChange={this.handleFormChange}
+                    handleEditProfile={this.handleEditProfile}
+                  />
+                )}
+              />
+              <Route
+                exact
+                path="/profile/:username/addrecipe"
+                render={props => (
+                  <AddRecipe
+                    currentUser={this.state.username}
+                    handleFormSubmit={this.handleRecipeSubmit}
+                  />
+                )}
+              />
+              <Route
+                exact
+                path="/search"
+                render={props => (
+                  <SearchResults
+                    userResults={this.state.usersearchresults}
+                    recipeResults={this.state.recipesearchresults}
+                    {...props}
+                  />
+                )}
               />
             </>
-          )}
-        />
-        <Route
-          exact
-          path="/recipe/:recipename"
-          render={props => (
-            <>
-              <Recipe {...props} showRecipe={this.state.recipes} />
-            </>
-          )}
-        />
-        <Route
-          exact
-          path="/profile/:username/stars"
-          render={props => <Stars currentUser={this.state.username} />}
-        />
-        <Route
-          exact
-          path="/profile/:username/edit"
-          render={props => (
-            <EditProfilePg
-              userData={this.state}
-              userId={
-                this.state.users.filter(
-                  user => user.username === this.state.username
-                )[0]
-              }
-              handleFormChange={this.handleFormChange}
-              handleEditProfile={this.handleEditProfile}
+          ) : (
+            <Route
+              path={[
+                "/feed",
+                "/profile/:username",
+                "/recipe/:recipename",
+                "/profile/:username/saved",
+                "/profile/:username/edit",
+                "/profile/:username/addrecipe",
+                "/search/:query"
+              ]}
+              render={props => <NotAuth />}
             />
           )}
-        />
-        <Route
-          exact
-          path="/profile/:username/addrecipe"
-          render={props => <AddRecipe />}
-        />
+          <Route render={props => <DoesntExist />} />
+        </Switch>
       </Router>
     );
   }
